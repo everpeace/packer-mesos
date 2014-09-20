230aaf3e1d6219bc9a4e314404d463ff2474f434
Shingo Omura <everpeace@gmail.com>
2014-09-20 02:28:01 -0700

    Versions of Chronos and Marathon to be installed should be parameterized. fix #2

--- scripts/common/install_chronos.sh
+++ scripts/common/install_chronos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get -y install default-jre-headless unzip curl libcurl3
-sudo curl -sSfL http://downloads.mesosphere.io/chronos/chronos-2.1.0_mesos-0.14.0-rc4.tgz --output /tmp/chronos.tgz
+sudo curl -sSfL http://downloads.mesosphere.io/chronos/chronos-$CHRONOS_VERSION.tgz --output /tmp/chronos.tgz
 mkdir -p /opt && cd /opt && tar xzf /tmp/chronos.tgz
--- scripts/common/install_marathon.sh
+++ scripts/common/install_marathon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get -y install default-jre-headless unzip curl libcurl3
-curl -sSfL http://downloads.mesosphere.io/marathon/v0.7.0-RC2/marathon-0.7.0-RC2.tgz --output /tmp/marathon.tgz
+curl -sSfL http://downloads.mesosphere.io/marathon/v$MARATHON_VERSION/marathon-$MARATHON_VERSION.tgz --output /tmp/marathon.tgz
 mkdir -p /opt && cd /opt && tar xzf /tmp/marathon.tgz
