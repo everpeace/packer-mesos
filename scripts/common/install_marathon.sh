2390143283306e61aa251a9bb7004afcadc48652
Shingo Omura <everpeace@gmail.com>
2014-09-26 21:04:23 -0700

    make marathon installation path be /opt/marathon/.

--- scripts/common/install_marathon.sh
+++ scripts/common/install_marathon.sh
@@ -3,3 +3,4 @@
 apt-get -y install default-jre-headless unzip curl libcurl3
 curl -sSfL http://downloads.mesosphere.io/marathon/v$MARATHON_VERSION/marathon-$MARATHON_VERSION.tgz --output /tmp/marathon.tgz
 mkdir -p /opt && cd /opt && tar xzf /tmp/marathon.tgz
+mv marathon-$MARATHON_VERSION marathon
